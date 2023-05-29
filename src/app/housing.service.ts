b5920fa7fab9b3475f5795b4e7fd98189e257362
sadat.malik <sadat.malik@me.com>
2023-05-29 20:31:28 +0100

    update the housing service with async functions to call json server endpoints

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -5,116 +5,18 @@ import { HousingLocation } from "./housinglocation";
   providedIn: 'root'
 })
 export class HousingService {
-  housingLocationList: HousingLocation[] = [
-    {
-      id: 0,
-      name: 'Acme Fresh Start Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: '/assets/digital-marketing-agency-ntwrk-g39p1kDjvSY-unsplash.jpg',
-      availableUnits: 4,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 1,
-      name: 'A113 Transitional Housing',
-      city: 'Santa Monica',
-      state: 'CA',
-      photo: '/assets/fomstock-4ojhpgKpS68-unsplash.jpg',
-      availableUnits: 0,
-      wifi: false,
-      laundry: true
-    },
-    {
-      id: 2,
-      name: 'Warm Beds Housing Support',
-      city: 'Juneau',
-      state: 'AK',
-      photo: '/assets/greg-rivers-rChFUMwAe7E-unsplash.jpg',
-      availableUnits: 1,
-      wifi: false,
-      laundry: false
-    },
-    {
-      id: 3,
-      name: 'Homesteady Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: '/assets/phil-hearing-IYfp2Ixe9nM-unsplash.jpg',
-      availableUnits: 1,
-      wifi: true,
-      laundry: false
-    },
-    {
-      id: 4,
-      name: 'Happy Homes Group',
-      city: 'Gary',
-      state: 'IN',
-      photo: '/assets/r-architecture-2gDwlIim3Uw-unsplash.jpg',
-      availableUnits: 1,
-      wifi: true,
-      laundry: false
-    },
-    {
-      id: 5,
-      name: 'Hopeful Apartment Group',
-      city: 'Oakland',
-      state: 'CA',
-      photo: '/assets/sieuwert-otterloo-aren8nutd1Q-unsplash.jpg',
-      availableUnits: 2,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 6,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo: '/assets/johnson-johnson-U6Q6zVDgmSs-unsplash.jpg',
-      availableUnits: 5,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 7,
-      name: 'Hopeful Housing Solutions',
-      city: 'Oakland',
-      state: 'CA',
-      photo: '/assets/stephan-bechert-yFV39g6AZ5o-unsplash.jpg',
-      availableUnits: 2,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 8,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo: '/assets/scott-webb-1ddol8rgUH8-unsplash.jpg',
-      availableUnits: 10,
-      wifi: false,
-      laundry: false
-    },
-    {
-      id: 9,
-      name: 'Capital Safe Towns',
-      city: 'Portland',
-      state: 'OR',
-      photo: '/assets/jacques-bopp-Hh18POSx5qk-unsplash.jpg',
-      availableUnits: 6,
-      wifi: true,
-      laundry: true
-    }
-  ];
+  url = 'http://localhost:3000/locations';
+
   constructor() { }
 
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
   }
 
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
   }
 
   submitApplication(firstName: string, lastName: string, email: string) {
