fb4b001c06bfac3864fc234cc186d1b27d190e25
sadat.malik <sadat.malik@me.com>
2023-05-29 17:40:13 +0100

    add a variable to hold filtered lists (filtered by search criteria)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,9 +17,11 @@ import { HousingService } from "../housing.service";
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
 
 }
