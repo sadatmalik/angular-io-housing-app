af85042e4c36bf4be950f7482e84be741ad6c2ae
sadat.malik <sadat.malik@me.com>
2023-05-29 17:43:41 +0100

    implement the filter results by city function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,4 +24,13 @@ export class HomeComponent {
     this.filteredLocationList = this.housingLocationList;
   }
 
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
 }
